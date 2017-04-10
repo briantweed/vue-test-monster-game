e05320038d79357f87cb1bdb416df2066e2eb123
BrianInstagiv <brian@instagiv.com>
2017-04-10 11:06:26 +0100

    no message

--- app.js
+++ app.js
@@ -1,119 +1,126 @@
 new Vue({
 
-    el: '#app',
+	el: '#app',
 
-    data: {
+	data: {
 
-        gameIsRunning: false,
-        playerHealth: 0,
-        monsterHealth: 0,
-        playerColor: 'green',
-        monsterColor: 'green',
-        points: 0,
-        round: 0,
-        turns: [],
-        finalMessage: '',
+		gameIsRunning: false,
+		playerHealth: 0,
+		monsterHealth: 0,
+		playerColor: 'green',
+		monsterColor: 'green',
+		points: 0,
+		round: 0,
+		turns: [],
+		finalMessage: `
+			<ul>
+				<li>Each normal attack earns you 1 point</li>
+				<li>Healing requires 3 points</li>
+				<li>Special Attacks require 6 points</li>
+			</ul>
+		`,
 
-    },
+	}, // data
 
-    watch: {
+	watch: {
 
-    	playerHealth(value) {
-    		this.playerColor = this.healthBarColor(value);
-    	},
+		playerHealth(value) {
+			this.playerColor = this.healthBarColor(value);
+		},
 
-    	monsterHealth(value) {
-    		this.monsterColor = this.healthBarColor(value);
-    	},
+		monsterHealth(value) {
+			this.monsterColor = this.healthBarColor(value);
+		},
 
-    },
+	}, // watch
 
-    methods: {
+	methods: {
 
-        startGame: function() {
-            this.gameIsRunning = true;
-            this.playerHealth = 100;
-            this.monsterHealth = 100;
-            this.playerColor = 'green';
-            this.monsterColor = 'green';
-            this.finalMessage = '',
-            this.round = 1;
-            this.points = 0;
-            this.turns = [];
-        },
+		startGame: function() {
+			this.gameIsRunning = true;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+			this.playerColor = 'green';
+			this.monsterColor = 'green';
+			this.finalMessage = '',
+			this.round = 1;
+			this.points = 0;
+			this.turns = [];
+		},
 
-        attack: function() {
-            this.playerAttacks();
-            if(this.checkWin()) return;
-            this.monsterAttacks();
+		attack: function() {
+			this.playerAttacks();
 			if(this.checkWin()) return;
-            this.points++;
-            this.round++;
-        },
+			this.monsterAttacks();
+			if(this.checkWin()) return;
+			this.points++;
+			this.round++;
+		},
 
-        specialAttack: function () {
-            this.inflictDamage(10, 20, true);
-            this.points -=6;
-            this.checkWin();
-            this.round++;
-        },
-        
-        heal: function() {
-            this.playerHealth = this.playerHealth + 10 > 100 ? 100 : this.playerHealth += 10;
-            this.points -=3;
-            this.recordAction('Player heals for 10', true);
-            this.round++;
-        },
+		specialAttack: function () {
+			this.inflictDamage(10, 20, true);
+			this.points -=6;
+			this.checkWin();
+			this.round++;
+		},
 
-        giveUp: function() {
-            this.gameIsRunning = false;
-        },
+		heal: function() {
+			this.playerHealth = this.playerHealth + 10 > 100 ? 100 : this.playerHealth += 10;
+			this.points -=3;
+			this.recordAction('Player heals for 10', true);
+			this.round++;
+		},
 
-        playerAttacks: function() {
-            this.inflictDamage(3, 10, true);
-        },
+		giveUp: function() {
+			this.gameIsRunning = false;
+		},
 
-        monsterAttacks: function() {
-            this.inflictDamage(5, 12, false);
-        },
+		playerAttacks: function() {
+			this.inflictDamage(3, 10, true);
+		},
 
-        inflictDamage: function(min, max, isPlayer) {
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            if(isPlayer) {
-                this.monsterHealth -= damage;
-                this.recordAction('Player hits Monster for ' + damage + ' points', isPlayer);
-            }
-            else {
-                this.playerHealth -= damage;
-                this.recordAction('Monster hits Player for ' + damage + ' points', isPlayer);
-            }
-        },
+		monsterAttacks: function() {
+			this.inflictDamage(5, 12, false);
+		},
 
-        healthBarColor: function(num) {
-            return num <= 15 ? 'red' : ( num <= 40 ? 'darkorange' : 'green' );
-        },
+		inflictDamage: function(min, max, isPlayer) {
+			var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+			if(isPlayer) {
+				this.monsterHealth -= damage;
+				this.recordAction('Player hits Monster for ' + damage + ' points', isPlayer);
+			}
+			else {
+				this.playerHealth -= damage;
+				this.recordAction('Monster hits Player for ' + damage + ' points', isPlayer);
+			}
+		},
 
-        recordAction: function(text, isPlayer) {
-            this.turns.unshift({
-                isPlayer: isPlayer,
-                text: this.round + ') ' + text
-            });
-        },
-        
-        checkWin: function() {
+		healthBarColor: function(num) {
+			return num <= 15 ? 'red' : ( num <= 40 ? 'darkorange' : 'green' );
+		},
+
+		recordAction: function(text, isPlayer) {
+			this.turns.unshift({
+				isPlayer: isPlayer,
+				text: this.round + ') ' + text
+			});
+		},
+
+		checkWin: function() {
 			if(this.monsterHealth <= 0) {
 				this.monsterHealth = 0;
 				this.gameIsRunning = false;
-				this.finalMessage = 'Player Wins';
+				this.finalMessage = '<h1>Player Wins</h1>';
 				return true;
 			}
 			else if(this.playerHealth <= 0) {
 				this.playerHealth = 0;
 				this.gameIsRunning = false;
-				this.finalMessage = 'Monster Wins';
+				this.finalMessage = '<h1>Monster Wins</h1>';
 				return true;
 			}
-        }
-    }
+		}
 
-});
+	} // methods
+
+}); // vue
